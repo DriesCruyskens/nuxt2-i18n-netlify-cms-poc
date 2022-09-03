a4069ceb83c2696d94a8c66f52c00d1cc1decba8
Dries Cruyskens <dries.cruyskens@gmail.com>
2022-09-03 18:43:44 +0200

    init

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: "static",
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  router: {
+    trailingSlash: true,
+  },
+};
